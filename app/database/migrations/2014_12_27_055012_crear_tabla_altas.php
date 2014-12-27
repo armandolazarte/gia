420b5d5437557de2ec9dd7a90930b3961307796d
Samuel Mercado <samuel.mg@gmx.com>
2014-12-27 06:06:52 +0000

    Migración de tabla Altas

---
+++ app/database/migrations/2014_12_27_055012_crear_tabla_altas.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaAltas extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('altas', function($t)
+		{
+			$t->increments('id');
+			$t->integer('doc_id');
+			$t->string('tipo', 20);
+			$t->integer('egreso_id')->unsigned();
+			//$t->foreign('egreso_id')->references('id')->on('egresos');
+			$t->integer('urg_id')->unsigned();
+			$t->foreign('urg_id')->references('id')->on('urgs');
+			$t->integer('debito')->unsigned();
+			$t->string('estatus', 20);
+			$t->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('altas');
+	}
+
+}
