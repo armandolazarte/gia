a73c4ab12ac554095aad43ee9921fc475fb2c41b
Samuel Mercado <samuel.mg@gmx.com>
2015-01-09 17:00:40 +0000

    Estructura mínima de layout

---
+++ app/views/layouts/base.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        @include('partials.head')
+    </head>
+    
+    <body>
+        
+        <!-- WRAPPER -->
+        <div>
+        
+        @yield('contenido')
+        
+        </div>
+        <!-- END WRAPPER -->
+        
+    </body>
+</html>
