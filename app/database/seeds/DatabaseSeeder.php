ea97e7fea3adc737b09550524342ccfaca58bf2b
Samuel Mercado <samuel.mg@gmx.com>
2015-01-20 14:16:39 -0600

    Modulos Table Seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder {
 		// $this->call('UserTableSeeder');
 		$this->call('RolesTableSeeder');
 		$this->call('TestUsersTableSeeder');
+		$this->call('ModulosTableSeeder');
 	}
 
 }
