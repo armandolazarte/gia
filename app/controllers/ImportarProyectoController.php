09276772edb9198459170c955b1f2c299e57e67f
Samuel Mercado <samuel.mg@gmx.com>
2015-01-30 15:17:14 -0600

    Desarrollo parcial para guardar información importada del proyecto

--- app/controllers/ImportarProyectoController.php
+++ app/controllers/ImportarProyectoController.php
@@ -34,7 +34,7 @@ class ImportarProyectoController extends BaseController
          */
 
         $data['proyecto'] = $importador->proy;
-        $data['nombre'] = $importador->nombre;
+        $data['d_proyecto'] = $importador->d_proyecto;
         $data['urg'] = $importador->urg;
         $data['d_urg'] = $importador->d_urg;
         $data['fondo'] = $importador->fondo;
@@ -47,10 +47,37 @@ class ImportarProyectoController extends BaseController
     public function store(){
         $importador = new \Gia\Classes\ImportadorProyecto('/uploads/proyectos/temp.txt');
         $importador->extraer();
+
+        $urg = Urg::whereUrg($importador->urg)-get();
+        $tipo_proyecto = TipoProyecto::whereTipoProyecto('P3E-LGCG')->get();
+        $fondo = Fondo::whereDFondo($importador->fondo)->get();
+
         $proyecto = new Proyecto();
         $proyecto->proyecto = $importador->proy;
+        $proyecto->d_proyecto = $importador->d_proyecto;
+        $proyecto->monto = $importador->monto_proy;
+        $proyecto->urg_id = $urg[0]->id;
+        $proyecto->tipo_proyecto_id = $tipo_proyecto[0]->id;
+        //@todo Determinar año
+        //@todo Insertar datos @fondo_proyecto
+        $proyecto->save();
 
-        //$proyecto->save();
+        //@todo Insertar objetivos
+        //@todo Insertar actividades
+        //@todo Exraer d_rm
+
+        foreach($importador->arr_recursos as $partida => $val){
+            $cog = Cog::whereCog($val['cog'])->get();
+            $rm = new Rm();
+            $rm->rm = $partida;
+            $rm->proyecto_id = $proyecto->id;
+            $rm->cog_id = $cog[0]->id;
+            $rm->fondo_id = $fondo[0]->id;
+            $rm->monto = $val['monto'];
+            $rm->d_rm = "";
+            $rm->save();
+        }
+
         return \Illuminate\Support\Facades\Redirect::action('ProyectosController@show', $proyecto->proyecto);
     }
 }
