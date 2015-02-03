8016be1b3da694b8e12bb77b15d41c50acb83fe6
Samuel Mercado <samuel.mg@gmx.com>
2015-02-03 17:30:55 -0600

    Importación de proyectos P3E-LGCG a partir de reporte PDF

--- app/Gia/Classes/ImportadorProyecto.php
+++ app/Gia/Classes/ImportadorProyecto.php
@@ -16,24 +16,25 @@ class ImportadorProyecto
     var $text;
     var $proy;
     var $fondo;
+    var $d_fondo;
     var $d_proyecto;
     var $urg;
     var $d_urg;
     var $monto_proy;
     var $t_rm;
-    var $rm_no, $rm_cta, $rm_fondo, $rm_monto, $rm_desc, $objetivo, $actividad_no;
     var $buscar_bms;
-    var $bms;
-    var $promep;
-    var $fip;
-
-    var $aaaa_db;
     var $arr_partida;
+    var $t_recursos;
     var $arr_cog;
+    var $arr_drm;
     var $arr_monto;
     var $arr_rm_objetivo;
+    var $arr_rm_d_objetivo;
     var $monto_total_cc;
     var $arr_recursos;
+    var $aaaa;
+    var $inicio;
+    var $fin;
 
     public function __construct($file)
     {
@@ -54,6 +55,7 @@ class ImportadorProyecto
         $this->extraerNombreProyecto();
         $this->extraerUrg();
         $this->extraerDescUrg();
+        $this->extraerFechas();
         $this->extraerFondo();
         $this->extraerMontoProyecto();
 
@@ -64,8 +66,10 @@ class ImportadorProyecto
         if ( count($this->arr_partida) == count($this->arr_cog) && count($this->arr_partida) == count($this->arr_monto) ) {
             for ($i = 0; $i < count($this->arr_partida); $i++) {
                 $this->arr_recursos[$this->arr_partida[$i]]['cog'] = $this->arr_cog[$i];
+                $this->arr_recursos[$this->arr_partida[$i]]['d_rm'] = $this->arr_drm[$i];
                 $this->arr_recursos[$this->arr_partida[$i]]['monto'] = $this->arr_monto[$i];
                 $this->arr_recursos[$this->arr_partida[$i]]['objetivo'] = $this->arr_rm_objetivo[$this->arr_partida[$i]];
+                $this->arr_recursos[$this->arr_partida[$i]]['d_objetivo'] = $this->arr_rm_d_objetivo[$this->arr_partida[$i]];
             }
         }
     }
@@ -83,7 +87,7 @@ class ImportadorProyecto
         $t_dproy = strstr($this->text, "Nombre del Proyecto: ");
         $posision_ures = strpos($t_dproy, "URG:");
         $t_dproy = substr($t_dproy, 21, $posision_ures-22 );
-        $this->d_proyecto = utf8_encode($t_dproy);
+        $this->d_proyecto = rtrim(utf8_encode($t_dproy));
     }
 
     private function extraerUrg()
@@ -101,6 +105,23 @@ class ImportadorProyecto
         $this->d_urg = substr(utf8_encode($t_durg), 16, $pos_finlinea - 16);
     }
 
+    private function extraerFechas(){
+        $t_fechas = strstr($this->text, utf8_decode("o: "));
+        $pattern = '/[0-9][0-9][0-9][0-9]/';
+        preg_match($pattern, $t_fechas, $matches);
+        $this->aaaa = $matches[0];
+
+        $t_inicio = strstr($this->text, "Inicio: ");
+        $pattern = '/[0-9][0-9]\/[0-9][0-9]\/[2][0-9][0-9][0-9]/';
+        preg_match($pattern, $t_inicio, $matches);
+        $this->inicio = $matches[0];
+
+        $t_fin = strstr($this->text, "Fin: ");
+        $pattern = '/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/';
+        preg_match($pattern, $t_fin, $matches);
+        $this->fin = $matches[0];
+    }
+
     private function extraerFondo()
     {
         $t_fondo = strstr($this->text, "Fondo: ");
@@ -111,9 +132,14 @@ class ImportadorProyecto
         $fondo_txt = str_replace(PHP_EOL, '', $fondo_txt);
         $fondo_txt = str_replace('      ', '', $fondo_txt);
         $fondo_txt = utf8_encode($fondo_txt);
-        /**
-         * @todo Verificar existencia del fondo
-         */
+
+        $fondo = \Fondo::whereDFondo($fondo_txt)->get();
+        if ( count($fondo) > 0 ){
+            $this->fondo = $fondo[0]->fondo;
+        }
+        //@todo Agregar mensaje y bandera de error cuando no se encuentre el fondo
+        //@todo Agregar fomulario emergente para dar de alta el fondo
+        $this->d_fondo = $fondo_txt;
     }
 
     private function extraerMontoProyecto()
@@ -192,6 +218,13 @@ class ImportadorProyecto
             preg_match_all($patron_no_obj, $t_objetivos, $arr_match_obj);
             $no_obj = $arr_match_obj[0][0];
 
+            //Obtener Descripción del Objetivo
+            $t_d_objetivo = strstr($t_objetivos, utf8_decode("Alineación PDI"), true);
+            $posision_obj = strpos($t_d_objetivo, $no_obj);
+            $t_d_objetivo = trim($t_d_objetivo);
+            $t_d_objetivo = substr($t_d_objetivo, $posision_obj+8);
+            $d_objetivo = utf8_encode($t_d_objetivo);
+
             //Obtener RMs del objetivo
             $pos_siguiente_obj = strpos($t_objetivos, "Objetivo Particular", 20);
             if ( $pos_siguiente_obj != false ) {
@@ -209,6 +242,7 @@ class ImportadorProyecto
                 if ( $rm != $this->proy )
                 {
                     $this->arr_rm_objetivo[$rm] = $no_obj;
+                    $this->arr_rm_d_objetivo[$rm] = $d_objetivo;
                 }
             }
         }
--- app/controllers/ImportarProyectoController.php
+++ app/controllers/ImportarProyectoController.php
@@ -33,12 +33,19 @@ class ImportarProyectoController extends BaseController
          * @todo Verificar existencia de todos los campos
          */
 
+        $verifica_proy = Proyecto::whereProyecto($importador->proy)->get();
+
+        $data['verifica_proy'] = $verifica_proy;
         $data['proyecto'] = $importador->proy;
         $data['d_proyecto'] = $importador->d_proyecto;
         $data['urg'] = $importador->urg;
         $data['d_urg'] = $importador->d_urg;
         $data['fondo'] = $importador->fondo;
+        $data['d_fondo'] = $importador->d_fondo;
         $data['monto'] = $importador->monto_proy;
+        $data['aaaa'] = $importador->aaaa;
+        $data['inicio'] = $importador->inicio;
+        $data['fin'] = $importador->fin;
         $data['partidas'] = $importador->arr_recursos;
 
         return View::make('admin.proyectos.importPreview')->with($data);
@@ -48,29 +55,49 @@ class ImportarProyectoController extends BaseController
         $importador = new \Gia\Classes\ImportadorProyecto('/uploads/proyectos/temp.txt');
         $importador->extraer();
 
-        $urg = Urg::whereUrg($importador->urg)-get();
-        $tipo_proyecto = TipoProyecto::whereTipoProyecto('P3E-LGCG')->get();
-        $fondo = Fondo::whereDFondo($importador->fondo)->get();
+
+
+        $urg = Urg::whereUrg($importador->urg)->get(array('id'));
+        $fondo = Fondo::whereFondo($importador->fondo)->get(array('id'));
+
+        $arr_inicio = explode("/", $importador->inicio);
+        $arr_fin = explode("/", $importador->fin);
 
         $proyecto = new Proyecto();
         $proyecto->proyecto = $importador->proy;
         $proyecto->d_proyecto = $importador->d_proyecto;
         $proyecto->monto = $importador->monto_proy;
         $proyecto->urg_id = $urg[0]->id;
-        $proyecto->tipo_proyecto_id = $tipo_proyecto[0]->id;
-        //@todo Determinar año
-        //@todo Insertar datos @fondo_proyecto
+        $proyecto->tipo_proyecto_id = 1;
+        $proyecto->aaaa = $importador->aaaa;
+        $proyecto->inicio = $arr_inicio[2]."-".$arr_inicio[1]."-".$arr_inicio[0];
+        $proyecto->fin = $arr_fin[2]."-".$arr_fin[1]."-".$arr_fin[0];
         $proyecto->save();
 
-        //@todo Insertar objetivos
-        //@todo Insertar actividades
-        //@todo Exraer d_rm
+        //Inserta datos @fondo_proyecto
+        $proyecto->fondos()->attach($fondo[0]->id);
 
+        $arr_objetivos = array();
         foreach($importador->arr_recursos as $partida => $val){
+            //Buscar objetivo en key del arreglo
+            $objetivo_id = array_search($val['objetivo'], $arr_objetivos);
+
+            //Si no se encuentra
+            if( empty($objetivo_id) ) {
+                $objetivo = new Objetivo();
+                $objetivo->objetivo = $val['objetivo'];
+                $objetivo->d_objetivo = $val['d_objetivo'];
+                $objetivo->save();
+                $objetivo_id = $objetivo->id;
+                $arr_objetivos[$objetivo->id] = $val['objetivo'];
+            }
+
             $cog = Cog::whereCog($val['cog'])->get();
             $rm = new Rm();
             $rm->rm = $partida;
             $rm->proyecto_id = $proyecto->id;
+            $rm->objetivo_id = $objetivo_id;
+            $rm->actividad_id = 1;
             $rm->cog_id = $cog[0]->id;
             $rm->fondo_id = $fondo[0]->id;
             $rm->monto = $val['monto'];
@@ -78,6 +105,6 @@ class ImportarProyectoController extends BaseController
             $rm->save();
         }
 
-        return \Illuminate\Support\Facades\Redirect::action('ProyectosController@show', $proyecto->proyecto);
+        return Redirect::action('ProyectosController@index');
     }
 }
