1eadb83789654054a1c9ba3401bf68eef7ca07a7
Samuel Mercado <samuel.mg@gmx.com>
2015-02-05 12:32:50 -0600

    Clase Acceso: determina proyectos a mostrar en función del usuario

---
+++ app/Gia/Classes/Acceso.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Gia\Classes;
+
+
+class Acceso
+{
+    var $proyectos;
+    var $user_id;
+
+    public function __construct()
+    {
+        $user = \Auth::user();
+        isset($user) ? $this->user_id = $user->id : $this->user_id = 0;
+    }
+
+    public function consultarProyectos()
+    {
+        $this->proyectos = \Proyecto::acceso($this->user_id)->get(array('id','proyecto','d_proyecto'));
+    }
+
+    public function getArrProyectos()
+    {
+        $this->consultarProyectos();
+
+        $arr_proyectos = array();
+        if (count($this->proyectos) > 0){
+            foreach($this->proyectos as $proyecto){
+                $arr_proyectos[$proyecto->id] = $proyecto->proyecto.' - '.$proyecto->d_proyecto;
+            }
+        } else {
+            $arr_proyectos[0] = 'No se ha dado de alta el acceso a los proyectos';
+        }
+        return $arr_proyectos;
+    }
+}
